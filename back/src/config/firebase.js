9a262e553a022c0308c9d8a9b0c460abbbfe1ca1
vnioa <nskfn02@gmail.com>
2024-11-29 23:01:56 +0900

    front 수정

--- back/src/config/firebase.js
+++ back/src/config/firebase.js
@@ -6,7 +6,7 @@ const serviceAccount = {
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -15,13 +15,13 @@ const serviceAccount = {
     client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
 };
 
-// Firebase 초기화
-admin.initializeApp({
+// Firebase Admin SDK 초기화
+const app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
 // FCM 메시징 인스턴스 생성
-const messaging = admin.messaging();
+const messaging = admin.messaging(app);
 
 // FCM 알림 전송 함수
 const sendPushNotification = async (token, title, body, data = {}) => {
@@ -66,7 +66,7 @@ const sendMulticastPushNotification = async (tokens, title, body, data = {}) => 
 };
 
 module.exports = {
-    admin,
+    app,
     messaging,
     sendPushNotification,
     sendMulticastPushNotification
